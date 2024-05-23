b6ecc81e3984fbe2ca7460e58bbca9b3f9a8a393
VitaliiPytymko <vitaliipytymko@gmail.com>
2024-05-23 23:03:25 +0300

    Добавьте файлы проекта.

---
+++ AsyncClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace AsyncClient
+{
+    internal class Program
+    {
+
+        static async Task  Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Loopback, 1234);
+            try
+            {
+                Console.WriteLine("Client has started");
+                await socket.ConnectAsync(endPoint);
+                string message = "Привіт, сервере!";
+                await socket.SendAsync(Encoding.UTF8.GetBytes(message), SocketFlags.None);
+
+                var buf = new byte[1024];
+                int bytesRead = await socket.ReceiveAsync(buf, SocketFlags.None);
+                string responseMessage = Encoding.UTF8.GetString(buf, 0, bytesRead);
+                Console.WriteLine($"o {DateTime.Now:HH:mm} " +
+                                  $"від {socket?.RemoteEndPoint?.ToString()} " +
+                                  $"отримано рядок : {responseMessage}");
+                socket?.Shutdown(SocketShutdown.Both);
+            }
+            catch (SocketException se)
+            {
+                await Console.Out.WriteLineAsync($"SocketException:{se.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            finally
+            {
+                socket.Close();
+            }
+            Console.WriteLine("\nНатисніть будь-яку клавішу для завершення...");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ AsyncServer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace AsyncServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var endPoint = new IPEndPoint(IPAddress.Loopback, 1234);
+            socket.Bind(endPoint);
+            socket.Listen(10);
+            Console.WriteLine("Server has started");
+
+            try
+            {
+
+                while (true)
+                {
+                    var client = socket.Accept();
+                    _ = HandleClientAsync(client);
+                }
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine($"SocketException: {se.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            finally
+            {
+                socket.Close();
+            }
+         
+        }
+
+        private static async Task HandleClientAsync(Socket client)
+        {
+            var buf = new byte[1024];
+            try
+            {
+                int bytesRead = await client.ReceiveAsync(buf, SocketFlags.None);
+                string receivedMessage = Encoding.UTF8.GetString(buf, 0, bytesRead);
+                Console.WriteLine($"o {DateTime.Now:HH:mm}" +
+                                  $" від {client?.RemoteEndPoint?.ToString()}" +
+                                  $" отримано рядок:{receivedMessage}");
+
+                string responseMessage = "Привіт, клієнте! ";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+                await client?.SendAsync(responseBytes, SocketFlags.None);
+                client?.Shutdown(SocketShutdown.Both);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in HandleClientAsync: {ex.Message}");
+            }
+
+            finally { client?.Close(); }
+
+         
+
+        }
+    }
+}
+
+        
+          
+        
+
+    
+
